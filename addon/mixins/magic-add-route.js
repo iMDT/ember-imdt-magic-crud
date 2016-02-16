7ccd00ab98155a71d5030a877eea96982e11f289
gcampes <gabrieldecampes@gmail.com>
2016-02-16 09:44:16 -0200

    Added rollback on save

--- addon/mixins/magic-add-route.js
+++ addon/mixins/magic-add-route.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 import MagicActionRoute from './magic-action-route';
 
-const {
-  getProperties
-} = Ember;
-
 export default Ember.Mixin.create(MagicActionRoute, {
   renderTemplate: function(){
     this.render('magic-crud/form');
@@ -12,5 +8,11 @@ export default Ember.Mixin.create(MagicActionRoute, {
 
   model(){
     return this.store.createRecord(this.get('modelName'));
+  },
+
+  actions:{
+    willTransition(){
+      this.get('currentModel').rollbackAttributes();
+    }
   }
 });
