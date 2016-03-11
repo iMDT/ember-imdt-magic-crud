df2ea6325bf1ce39f9d2f80c8a2e31141b324824
gcampes <gabrieldecampes@gmail.com>
2016-03-11 11:07:23 -0300

    Adds nested routes functionality on the route-mapper

--- addon/utils/magic-route-mapper.js
+++ addon/utils/magic-route-mapper.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
-  map: function(context, route) {
+  map: function(context, route, callback) {
     context.route(route, function() {
       this.route('new');
       this.route('edit', {
@@ -10,6 +10,9 @@ export default Ember.Object.extend({
       this.route('show', {
         path: 'show/:id'
       });
+      if(callback){
+        callback();
+      }
     });
   }
 });
